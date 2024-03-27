0ce652795e51230528e90d656b9b6e822c5762d7
Sergey Skotselyas <sergey.sckotcelias@yandex.ru>
2024-03-27 15:35:38 +0800

    project1: replace keyCode with key

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -1,15 +1,17 @@
 window.addEventListener('keydown', (event) => {
-  const keyCode = event.keyCode;
-  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+  const key = event.key.toLocaleLowerCase();
+  const audio = document.querySelector(`audio[data-key="${key}"]`);
   if (!audio) return;
-  const keyElement = document.querySelector(`.key[data-key="${keyCode}"]`);
+  const keyElement = document.querySelector(`.key[data-key="${key}"]`);
   keyElement.classList.add('playing');
   audio.currentTime = 0;
   audio.play();
 });
 
 window.addEventListener('keyup', (event) => {
-  const keyCode = event.keyCode;
-  const keyElement = document.querySelector(`.key[data-key="${keyCode}"]`);
-  keyElement.classList.remove('playing');
+  const key = event.key.toLocaleLowerCase();
+  const keyElement = document.querySelector(`.key[data-key="${key}"]`);
+  if (keyElement) {
+    keyElement.classList.remove('playing');
+  }
 });
