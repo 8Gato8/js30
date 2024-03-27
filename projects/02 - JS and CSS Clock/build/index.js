47353b2a9ffbecc0c7256884ff9b8e82eb7e909e
Sergey Skotselyas <sergey.sckotcelias@yandex.ru>
2024-03-27 17:03:52 +0800

    project2: add typing

--- projects/02 - JS and CSS Clock/build/index.js
+++ projects/02 - JS and CSS Clock/build/index.js
@@ -1,26 +1,22 @@
+"use strict";
 const hourHand = document.querySelector('.hour-hand');
 const minHand = document.querySelector('.min-hand');
 const secondHand = document.querySelector('.second-hand');
-
 function setDate() {
-  const now = new Date();
-
-  const hours = now.getHours();
-  const minutes = now.getMinutes();
-  const seconds = now.getSeconds();
-
-  [hourHand, minHand, secondHand].forEach(
-    (el) => (el.style.transition = seconds === 0 ? 'none' : null),
-  );
-
-  const hoursDegrees = 30 * hours + 0.5 * minutes;
-  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-
-  const minutesDegrees = minutes * 6 + 0.1 * seconds;
-  minHand.style.transform = `rotate(${minutesDegrees}deg)`;
-
-  const secondsDegrees = seconds * 6;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    [hourHand, minHand, secondHand].forEach((el) => {
+        if (seconds === 0) {
+            el.style.transition = 'none';
+        }
+    });
+    const hoursDegrees = 30 * hours + 0.5 * minutes;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    const minutesDegrees = minutes * 6 + 0.1 * seconds;
+    minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    const secondsDegrees = seconds * 6;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 }
-
 setInterval(setDate, 1000);
---
+++ projects/02 - JS and CSS Clock/index.ts
@@ -0,0 +1,28 @@
+const hourHand = document.querySelector('.hour-hand') as HTMLDivElement;
+const minHand = document.querySelector('.min-hand') as HTMLDivElement;
+const secondHand = document.querySelector('.second-hand') as HTMLDivElement;
+
+function setDate() {
+  const now = new Date();
+
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+
+  [hourHand, minHand, secondHand].forEach((el) => {
+    if (seconds === 0) {
+      el.style.transition = 'none';
+    }
+  });
+
+  const hoursDegrees = 30 * hours + 0.5 * minutes;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+  const minutesDegrees = minutes * 6 + 0.1 * seconds;
+  minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const secondsDegrees = seconds * 6;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
