892c2b301c3af41ae3eb3702a923473a29449cf8
Sergey Skotselyas <sergey.sckotcelias@yandex.ru>
2024-03-27 16:49:32 +0800

    project1: add typing

--- projects/01 - JavaScript Drum Kit/build/index.js
+++ projects/01 - JavaScript Drum Kit/build/index.js
@@ -1,1 +1,20 @@
 "use strict";
+window.addEventListener('keydown', (event) => {
+    const key = event.key.toLocaleLowerCase();
+    const audio = document.querySelector(`audio[data-key="${key}"]`);
+    if (!audio)
+        return;
+    const keyElement = document.querySelector(`.key[data-key="${key}"]`);
+    if (keyElement) {
+        keyElement.classList.add('playing');
+    }
+    audio.currentTime = 0;
+    audio.play();
+});
+window.addEventListener('keyup', (event) => {
+    const key = event.key.toLocaleLowerCase();
+    const keyElement = document.querySelector(`.key[data-key="${key}"]`);
+    if (keyElement) {
+        keyElement.classList.remove('playing');
+    }
+});
