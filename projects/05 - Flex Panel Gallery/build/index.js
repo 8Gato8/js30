c34dd991fdfc11427fcf76c1b603bf88fe0f016b
Sergey Skotselyas <sergey.sckotcelias@yandex.ru>
2024-03-29 17:48:35 +0800

    project5: enhance logic

--- projects/05 - Flex Panel Gallery/build/index.js
+++ projects/05 - Flex Panel Gallery/build/index.js
@@ -1,17 +1,26 @@
 "use strict";
-const panels = document.querySelectorAll('.panel');
+const panels = document.querySelector('.panels');
 const toggleOpen = (event) => {
-    console.log(event.detail);
     if (event.detail < 2) {
         const target = event.target;
-        target.classList.toggle('open');
+        if (target.tagName === 'P') {
+            target.parentElement?.classList.toggle('open');
+        }
+        else {
+            target.classList.toggle('open');
+        }
     }
 };
 const toggleActive = (event) => {
     if (event.propertyName.includes('flex')) {
         const target = event.target;
-        target.classList.toggle('open-active');
+        if (target.tagName === 'P') {
+            target.parentElement?.classList.toggle('open-active');
+        }
+        else {
+            target.classList.toggle('open-active');
+        }
     }
 };
-panels.forEach((panel) => panel.addEventListener('click', toggleOpen));
-panels.forEach((panel) => panel.addEventListener('transitionend', toggleActive));
+panels.addEventListener('click', toggleOpen);
+panels.addEventListener('transitionend', toggleActive);
--- projects/05 - Flex Panel Gallery/index.ts
+++ projects/05 - Flex Panel Gallery/index.ts
@@ -1,19 +1,28 @@
-const panels = document.querySelectorAll('.panel') as NodeListOf<HTMLDivElement>;
+const panels = document.querySelector('.panels') as HTMLDivElement;
 
 const toggleOpen = (event: MouseEvent) => {
-  console.log(event.detail);
   if (event.detail < 2) {
-    const target = event.target as HTMLElement;
-    target.classList.toggle('open');
+    const target = event.target as HTMLDivElement;
+
+    if (target.tagName === 'P') {
+      target.parentElement?.classList.toggle('open');
+    } else {
+      target.classList.toggle('open');
+    }
   }
 };
 
 const toggleActive = (event: TransitionEvent) => {
   if (event.propertyName.includes('flex')) {
     const target = event.target as HTMLDivElement;
-    target.classList.toggle('open-active');
+
+    if (target.tagName === 'P') {
+      target.parentElement?.classList.toggle('open-active');
+    } else {
+      target.classList.toggle('open-active');
+    }
   }
 };
 
-panels.forEach((panel) => panel.addEventListener('click', toggleOpen));
-panels.forEach((panel) => panel.addEventListener('transitionend', toggleActive));
+panels.addEventListener('click', toggleOpen);
+panels.addEventListener('transitionend', toggleActive);
