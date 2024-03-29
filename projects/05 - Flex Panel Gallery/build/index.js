1b181f5d8c5169ba8440a2986b304a1fd703cc8a
Sergey Skotselyas <sergey.sckotcelias@yandex.ru>
2024-03-29 15:39:25 +0800

    projec5: add logic for opening panels

--- projects/05 - Flex Panel Gallery/build/index.js
+++ projects/05 - Flex Panel Gallery/build/index.js
@@ -1,1 +1,17 @@
 "use strict";
+const panels = document.querySelectorAll('.panel');
+const toggleOpen = (event) => {
+    console.log(event.detail);
+    if (event.detail < 2) {
+        const target = event.target;
+        target.classList.toggle('open');
+    }
+};
+const toggleActive = (event) => {
+    if (event.propertyName.includes('flex')) {
+        const target = event.target;
+        target.classList.toggle('open-active');
+    }
+};
+panels.forEach((panel) => panel.addEventListener('click', toggleOpen));
+panels.forEach((panel) => panel.addEventListener('transitionend', toggleActive));
--- projects/05 - Flex Panel Gallery/index.ts
+++ projects/05 - Flex Panel Gallery/index.ts
@@ -1,1 +1,19 @@
+const panels = document.querySelectorAll('.panel') as NodeListOf<HTMLDivElement>;
 
+const toggleOpen = (event: MouseEvent) => {
+  console.log(event.detail);
+  if (event.detail < 2) {
+    const target = event.target as HTMLElement;
+    target.classList.toggle('open');
+  }
+};
+
+const toggleActive = (event: TransitionEvent) => {
+  if (event.propertyName.includes('flex')) {
+    const target = event.target as HTMLDivElement;
+    target.classList.toggle('open-active');
+  }
+};
+
+panels.forEach((panel) => panel.addEventListener('click', toggleOpen));
+panels.forEach((panel) => panel.addEventListener('transitionend', toggleActive));
