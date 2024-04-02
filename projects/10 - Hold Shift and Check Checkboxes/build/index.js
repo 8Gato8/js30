e3a9b47866a1a1632f8df0e7e9e1748eae594d7d
Sergey Skotselyas <sergey.sckotcelias@yandex.ru>
2024-04-03 00:49:25 +0800

    project10: write logic for hold shift checkbox selection

--- projects/10 - Hold Shift and Check Checkboxes/build/index.js
+++ projects/10 - Hold Shift and Check Checkboxes/build/index.js
@@ -1,3 +1,19 @@
-'use strict';
-const inputs = document.querySelectorAll('input');
-console.table(inputs);
+"use strict";
+const checkboxes = document.querySelectorAll('input');
+let lastChecked;
+const handleCheck = (e) => {
+    const target = e.target;
+    let inBetween = false;
+    if (e.shiftKey && target.checked) {
+        checkboxes.forEach((checkbox) => {
+            if (checkbox === target || checkbox === lastChecked) {
+                inBetween = !inBetween;
+            }
+            if (inBetween) {
+                checkbox.checked = true;
+            }
+        });
+    }
+    lastChecked = target;
+};
+checkboxes.forEach((checkbox) => checkbox.addEventListener('click', handleCheck));
--- projects/10 - Hold Shift and Check Checkboxes/index.ts
+++ projects/10 - Hold Shift and Check Checkboxes/index.ts
@@ -1,3 +1,24 @@
-const inputs = document.querySelectorAll('input');
+const checkboxes = document.querySelectorAll('input');
 
-console.table(inputs);
+let lastChecked: HTMLInputElement;
+
+const handleCheck = (e: MouseEvent) => {
+  const target = e.target as HTMLInputElement;
+
+  let inBetween = false;
+  if (e.shiftKey && target.checked) {
+    checkboxes.forEach((checkbox) => {
+      if (checkbox === target || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+
+  lastChecked = target;
+};
+
+checkboxes.forEach((checkbox) => checkbox.addEventListener('click', handleCheck));
