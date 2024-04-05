d519ea291c01395fdf00d4cacdee5fb0e4d2e0cf
Sergey Skotselyas <sergey.sckotcelias@yandex.ru>
2024-04-05 23:46:58 +0800

    project13: write logic for images to slide while scrolling

--- projects/13 - Slide in on Scroll/build/index.js
+++ projects/13 - Slide in on Scroll/build/index.js
@@ -1,16 +1,34 @@
-"use strict";
-/* function debounce(func, wait = 20, immediate = true) {
-  let timeout;
-  return function () {
-    const context = this,
-      args = arguments;
-    var later = function () {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
+function debounce(func, wait = 20, immediate = true) {
+    let timeout;
+    return function () {
+        const context = this;
+        const args = arguments;
+        const later = function () {
+            timeout = null;
+            if (!immediate)
+                func.apply(context, args);
+        };
+        const callNow = immediate && !timeout;
+        if (timeout !== null) {
+            clearTimeout(timeout);
+        }
+        timeout = setTimeout(later, wait);
+        if (callNow)
+            func.apply(context, args);
     };
-    const callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-} */
+}
+const sliderImages = document.querySelectorAll('.slide-in');
+const checkSlide = () => {
+    sliderImages.forEach((sliderImage) => {
+        const slideInAt = sliderImage.offsetTop - sliderImage.height;
+        const imageBottom = sliderImage.offsetTop + sliderImage.height * 2;
+        const isShown = window.scrollY >= slideInAt && window.scrollY < imageBottom;
+        if (isShown) {
+            sliderImage.classList.add('active');
+        }
+        else {
+            sliderImage.classList.remove('active');
+        }
+    });
+};
+window.addEventListener('scroll', debounce(checkSlide, 10));
--- projects/13 - Slide in on Scroll/index.ts
+++ projects/13 - Slide in on Scroll/index.ts
@@ -1,15 +1,36 @@
-/* function debounce(func, wait = 20, immediate = true) {
-  let timeout;
+function debounce(func: any, wait = 20, immediate = true) {
+  let timeout: undefined | number | null;
   return function () {
-    const context = this,
-      args = arguments;
-    var later = function () {
+    const context = this;
+    const args = arguments;
+    const later = function () {
       timeout = null;
       if (!immediate) func.apply(context, args);
     };
     const callNow = immediate && !timeout;
-    clearTimeout(timeout);
+    if (timeout !== null) {
+      clearTimeout(timeout);
+    }
     timeout = setTimeout(later, wait);
     if (callNow) func.apply(context, args);
   };
-} */
+}
+
+const sliderImages = document.querySelectorAll('.slide-in') as NodeListOf<HTMLImageElement>;
+
+const checkSlide = () => {
+  sliderImages.forEach((sliderImage) => {
+    const slideInAt = sliderImage.offsetTop - sliderImage.height;
+    const imageBottom = sliderImage.offsetTop + sliderImage.height * 2;
+
+    const isShown = window.scrollY >= slideInAt && window.scrollY < imageBottom;
+
+    if (isShown) {
+      sliderImage.classList.add('active');
+    } else {
+      sliderImage.classList.remove('active');
+    }
+  });
+};
+
+window.addEventListener('scroll', debounce(checkSlide, 10));
