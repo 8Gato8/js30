19e5c26b507933f074d0dea30336e0ed47c1b515
Sergey Skotselyas <sergey.sckotcelias@yandex.ru>
2024-04-07 22:23:24 +0800

    project18: calc fucntion to count time

---
+++ projects/18 - Adding Up Times with Reduce/index.ts
@@ -0,0 +1,24 @@
+type TVideos = (videos: HTMLLIElement[]) => string;
+const videos = Array.from(document.querySelectorAll('li'));
+
+const calcTotalTime: TVideos = (videos) => {
+  let totalHours: number = 0;
+  let totalMinutes: number = 0;
+  let totalSeconds: number = 0;
+
+  for (let video of videos) {
+    const time = video.dataset.time;
+    const [minutes, seconds] = time.split(':').map(parseFloat);
+
+    totalSeconds += minutes * 60 + seconds;
+  }
+
+  totalHours = Math.floor(totalSeconds / 3600);
+  totalSeconds %= 3600;
+  totalMinutes = Math.floor(totalSeconds / 60);
+  totalSeconds %= 60;
+
+  return `Total time is ${totalHours} hours, ${totalMinutes} minutes and ${totalSeconds} seconds`;
+};
+
+console.log(calcTotalTime(videos));
