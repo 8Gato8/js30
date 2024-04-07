dfec56c8d5836386a4f817562c8d3edcd99dedcf
Sergey Skotselyas <sergey.sckotcelias@yandex.ru>
2024-04-07 18:40:30 +0800

    project17: write sort logic for bands array

--- projects/17 - Sort Without Articles/build/index.js
+++ projects/17 - Sort Without Articles/build/index.js
@@ -1,3 +1,4 @@
+const bandsElement = document.querySelector('#bands');
 const bands = [
     'The Plot in You',
     'The Devil Wears Prada',
@@ -13,3 +14,12 @@ const bands = [
     'Anywhere But Here',
     'An Old Dog',
 ];
+const strip = (str) => str.replace(/^(a |an |the )/i, '').trim();
+const sortArray = (bands) => bands.sort((a, b) => (strip(a) > strip(b) ? 1 : -1));
+const createBandsList = (bands, bandsElement) => {
+    const sortedBandsArray = sortArray(bands);
+    sortedBandsArray.forEach((b) => {
+        bandsElement.insertAdjacentHTML(`beforeend`, `<li>${b}</li>`);
+    });
+};
+createBandsList(bands, bandsElement);
--- projects/17 - Sort Without Articles/index.ts
+++ projects/17 - Sort Without Articles/index.ts
@@ -1,3 +1,9 @@
+type TModifyArray<T> = (bands: T[]) => T[];
+type TStrip<T> = (str: T) => T;
+type TCreateBandsList<T> = (bands: T[], bandsElement: HTMLUListElement) => void;
+
+const bandsElement = document.querySelector('#bands') as HTMLUListElement;
+
 const bands = [
   'The Plot in You',
   'The Devil Wears Prada',
@@ -13,3 +19,17 @@ const bands = [
   'Anywhere But Here',
   'An Old Dog',
 ];
+
+const strip: TStrip<string> = (str) => str.replace(/^(a |an |the )/i, '').trim();
+
+const sortArray: TModifyArray<string> = (bands) =>
+  bands.sort((a, b) => (strip(a) > strip(b) ? 1 : -1));
+
+const createBandsList: TCreateBandsList<string> = (bands, bandsElement) => {
+  const sortedBandsArray = sortArray(bands);
+  sortedBandsArray.forEach((b) => {
+    bandsElement.insertAdjacentHTML(`beforeend`, `<li>${b}</li>`);
+  });
+};
+
+createBandsList(bands, bandsElement);
