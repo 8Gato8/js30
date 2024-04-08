f6997c92cd700f28ebab7adc30b784d475c56132
Sergey Skotselyas <sergey.sckotcelias@yandex.ru>
2024-04-08 15:51:46 +0800

    project19: add initial structure

---
+++ projects/19 - Webcam Fun/index.ts
@@ -0,0 +1,5 @@
+const video = document.querySelector('.player') as HTMLVideoElement;
+const canvas = document.querySelector('.photo') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+const strip = document.querySelector('.strip') as HTMLDivElement;
+const snap = document.querySelector('.snap') as HTMLAudioElement;
