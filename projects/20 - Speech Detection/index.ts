11f1c819b09069131d3966e62d775fee9d8012fc
Sergey Skotselyas <sergey.sckotcelias@yandex.ru>
2024-04-09 22:58:51 +0800

    add script for browser sync

--- projects/20 - Speech Detection/index.ts
+++ projects/20 - Speech Detection/index.ts
@@ -1,1 +1,28 @@
-/* window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition; */
+const SpeechRecognition =
+  (window as any).SpeechRecognition || (window as any).webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p: HTMLParagraphElement;
+const addParagraph = () => {
+  p = document.createElement('p');
+  const words = document.querySelector('.words');
+  words.appendChild(p);
+};
+
+addParagraph();
+
+recognition.addEventListener('result', (e: any) => {
+  const transcript = Array.from(e.results)
+    .map((res) => res[0].transcript)
+    .join('');
+  p.textContent = transcript;
+  if (e.results[0].isFinal) {
+    addParagraph();
+  }
+});
+
+recognition.addEventListener('end', recognition.start);
+
+recognition.start();
