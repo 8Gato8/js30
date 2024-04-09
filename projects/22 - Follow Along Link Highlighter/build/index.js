dfc7bd4156f95ef9ecabf9ec7c589aa05874134e
Sergey Skotselyas <sergey.sckotcelias@yandex.ru>
2024-04-10 01:21:44 +0800

    project22: add span highligter logic

---
+++ projects/22 - Follow Along Link Highlighter/build/index.js
@@ -0,0 +1,21 @@
+const links = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.append(highlight);
+const highlightLink = (e) => {
+    const link = e.target;
+    const linkCoords = link.getBoundingClientRect();
+    const coords = {
+        width: linkCoords.width,
+        height: linkCoords.height,
+        top: linkCoords.top + window.scrollY,
+        left: linkCoords.left + window.scrollX,
+    };
+    let { width, height, top, left } = coords;
+    highlight.style.width = `${width}px`;
+    highlight.style.height = `${height}px`;
+    highlight.style.transform = `translate(${left}px, ${top}px)`;
+};
+links.forEach((l) => {
+    l.addEventListener('mouseenter', highlightLink);
+});
