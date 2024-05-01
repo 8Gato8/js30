003361404b24359ef107ba3a1d9909a141e20009
Sergey Scotcelias <sergey.sckotcelias@yandex.ru>
2024-05-01 23:56:03 +0800

    project30: add randomTime function

--- projects/30 - Whack A Mole/index.ts
+++ projects/30 - Whack A Mole/index.ts
@@ -1,3 +1,9 @@
 const holes = document.querySelectorAll('.hole') as NodeListOf<HTMLDivElement>;
 const scoreBoard = document.querySelector('.score') as HTMLSpanElement;
 const moles = document.querySelectorAll('.mole') as NodeListOf<HTMLDivElement>;
+
+type IRandomTime<T> = (min: T, max: T) => T;
+
+const randomTime: IRandomTime<number> = (min, max) => {
+  return Math.random() * (max - min) + min;
+};
