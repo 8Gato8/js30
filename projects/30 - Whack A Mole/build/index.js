fe8dd48306e1591f7c38fad93989f5975379e037
Sergey Scotcelias <sergey.sckotcelias@yandex.ru>
2024-05-02 00:03:17 +0800

    project30: add randomHole function

--- projects/30 - Whack A Mole/build/index.js
+++ projects/30 - Whack A Mole/build/index.js
@@ -1,3 +1,6 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+const randomTime = (min, max) => {
+    return Math.random() * (max - min) + min;
+};
--- projects/30 - Whack A Mole/index.ts
+++ projects/30 - Whack A Mole/index.ts
@@ -2,8 +2,14 @@ const holes = document.querySelectorAll('.hole') as NodeListOf<HTMLDivElement>;
 const scoreBoard = document.querySelector('.score') as HTMLSpanElement;
 const moles = document.querySelectorAll('.mole') as NodeListOf<HTMLDivElement>;
 
-type IRandomTime<T> = (min: T, max: T) => T;
+type TRandomTime<T> = (min: T, max: T) => T;
+/* type TRandomHole */
 
-const randomTime: IRandomTime<number> = (min, max) => {
+const randomTime: TRandomTime<number> = (min, max) => {
   return Math.random() * (max - min) + min;
+};
+
+const randomHole = (holes: NodeListOf<HTMLDivElement>) => {
+  const index = Math.random() * holes.length;
+  return holes[index];
 };
